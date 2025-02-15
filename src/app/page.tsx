4a98796acf27704ffab10a101d09b5f7b1c40981
Jared Tokuz <jtokuz@gmail.com>
2025-02-14 21:08:00 -0600

    revert to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 'use client';
 
-import PredictionMarket from '@/components/prediction-market/PredictionMarket';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <PredictionMarket />
-    </main>
-  );
-}
-
-// import PredictionMarketTest from '@/components/prediction-market-test/PredictionMarketTest';
+// import PredictionMarket from '@/components/prediction-market/PredictionMarket';
 
 // export default function Home() {
 //   return (
 //     <main className="min-h-screen bg-gray-50">
-//       <PredictionMarketTest />
+//       <PredictionMarket />
 //     </main>
 //   );
 // }
+
+import PredictionMarketTest from '@/components/prediction-market-test/PredictionMarketTest';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <PredictionMarketTest />
+    </main>
+  );
+}
